a78a0c052e3bcd03a3b5e911afc2a119533aea7c
unknown <efe@Emil-PC.2BM.DK>
2014-10-22 15:31:05 +0200

    TaskHelper and EasyStopwatch helper v1

---
+++ Async/TaskHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EsthaHelpers.Async
+{
+    /// <summary>
+    /// Class for help with running async tasks in a sync manner.
+    /// </summary>
+    public static class TaskHelper
+    {
+        public static void RunTaskSynchronously(this Task t)
+        {
+            var task = Task.Run(async () => await t);
+            task.Wait();
+        }
+
+        public static T RunTaskSynchronously<T>(this Task<T> t)
+        {
+            T res = default(T);
+            var task = Task.Run(async () => res = await t);
+            task.Wait();
+            return res;
+        }
+    }
+}
---
+++ Time/EasyStopwatch.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace EsthaHelpers.Time
+{
+    /// <summary>
+    /// Class for an easy-peasy use of stopwatches;
+    /// </summary>
+    public class EasyStopwatch
+    {
+        //DIctionary with all stopwatches started.
+        private static Dictionary<string, Stopwatch> easyStopwatchDictionary = new Dictionary<string, Stopwatch>();
+
+        public static void start(String whichTitle)
+        {
+            try
+            {
+                if (!easyStopwatchDictionary.ContainsKey(whichTitle))
+                {
+                    easyStopwatchDictionary.Add(whichTitle, new Stopwatch());
+                }
+
+                easyStopwatchDictionary[whichTitle].Start();
+            }
+            catch (Exception e)
+            {
+                Debug.Write("EsthaHelpers.Time Exception: " + e.ToString());
+            }
+        }
+
+        public static long inter(String whichTitle)
+        {
+            try
+            {
+                if (easyStopwatchDictionary.ContainsKey(whichTitle))
+                {
+                    Debug.Write(System.String.Format(
+                            "\"" + whichTitle + "\" stopwatch: " +
+                            easyStopwatchDictionary[whichTitle].Elapsed.ToString() +
+                            "\n"
+                    ));
+                    return  easyStopwatchDictionary[whichTitle].Elapsed.Ticks;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write("EsthaHelpers.Time Exception: " + e.ToString());
+            }
+            return 0;
+        }
+
+        public static long end__(String whichTitle)
+        {
+            try
+            {
+                if (easyStopwatchDictionary.ContainsKey(whichTitle))
+                {
+                    easyStopwatchDictionary[whichTitle].Stop();
+                    Debug.Write(System.String.Format(
+                            "\"" + whichTitle + "\" stopwatch: " +
+                            easyStopwatchDictionary[whichTitle].Elapsed.ToString() +
+                            "\n"
+                    ));
+                    long elapsetTicks = easyStopwatchDictionary[whichTitle].Elapsed.Ticks;
+                    easyStopwatchDictionary.Remove(whichTitle);
+                    return elapsetTicks;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write("EsthaHelpers.Time Exception: " + e.ToString());
+            }
+            return 0;
+        }
+
+    }
+}
