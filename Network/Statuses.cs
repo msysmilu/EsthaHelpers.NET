bbc621bcdbb581e4bccd82fc1c711d195e1d9a96
unknown <efe@Emil-PC.2BM.DK>
2014-12-05 16:14:30 +0100

    up

---
+++ Network/Statuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdCardPhotos2SuccessFactors.Helpers.EsthaHelpers.NET.Network
+{
+    public class Statuses
+    {
+        public static const String STATUS_OK = "ok";
+        public static const String STATUS_ERROR = "error";
+    }
+}
